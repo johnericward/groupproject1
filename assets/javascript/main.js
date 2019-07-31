34f01b9b26dcb74fcafcc9d7cbf21a33a2cc9f41
johnericward <johnericward@gmail.com>
2019-07-31 19:24:16 -0400

    cleared out davids search code so i wouldnt create conflicts and annotated my code with comments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,67 +1,11 @@
-/* 
-   User opens app
-   User inputs superhero name in search box 1, clicks "Enter" button for stats
-   User inputs superhero name in search box 2, clicks "Enter" button for stats
-   User clicks "VS" button for head to head matchup
-   Hero stats are compared 
-   Through algorhithm created of hero stats, a head to head matchup winner is determined
-   
-   Weather icon with superhero as icon will be located somewhere on screen
-   Hero icon will be assigned to weather condition (Heat, rain, snow, wind, hail, etc.)
-   This will run when user adds ZIP code 
-*/
+//===============================================================================================================
+//===================================== ERIC'S WEATHER STUFF ====================================================
+//===============================================================================================================
 
-// Your web app's Firebase configuration
-
-/*
-var firebaseConfig = {
-   apiKey: "AIzaSyB2EKfaYb0g1N82L2rYGALQ4e2RcKQTSas",
-   authDomain: "timesheet-91aa6.firebaseapp.com",
-   databaseURL: "https://timesheet-91aa6.firebaseio.com",
-   projectId: "timesheet-91aa6",
-   storageBucket: "timesheet-91aa6.appspot.com",
-   messagingSenderId: "1048436736797",
-   appId: "1:1048436736797:web:2e159bdff5e5fe69"
- };
- */
-
-// Initialize Firebase
-// ================================================================
-
-/*
- 
-firebase.initializeApp(firebaseConfig);
-
-var heroArray = [""];
-
-function searchSuperHero (superhero) {
-
-  var queryURL = "https://superheroapi.com/api/10157235138196007/search/name";
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      console.log(response);
-
-    }
-
-       */
-
-// WEATHER STUFF
-
-
-
-//     apiKey: c92d5aaad4a82504cb850e2089f5c6ff
-//     apiCall: api.openweathermap.org/data/2.5/weather?zip={zip code},{1}
-
-
-// weather.main
-// main.temp
-
-// This is our API key. Add your own API key between the ""
+// API Key for Open Weather Map
 var APIKey = "26418792bafdcd3fe320935f49fe3ba5";
 
+// array of objects to compare weather types returned by API and add in our own images of super heroes
 var imageData = [
   { type: "Rain", src: "assets/images/rain.png" },
   { type: "Snow", src: "assets/images/snow.png" },
@@ -73,43 +17,44 @@ var imageData = [
 ];
 
 
-// Here we are building the URL we need to query the database
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip={" + zipCode + "},{1}" + APIKey;
+// this js only runs when user clicks the submit ZIP button at top right of page
 $(document).on("click", ".weatherZipButton", function () {
 
+  // grabs whatever user types into input field and stores it in weatherZip variable
     var weatherZip = $(".weatherZipSearch").val().trim();
     console.log(weatherZip);
 
+  //this is API url using my API key and concatinating ZIP entered in the input field stored in weatherZIP variable
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + weatherZip + ",us&units=imperial&appid=" + APIKey;
   
-  
+  //these are to link the divs in the HTML with the js
   var weatherTempCSS = $(".weatherTemp")
   var weatherTypeCSS = $(".weatherType")
 
   var weatherImageDiv = $(".weatherImageDiv");
   
 
-    // We then created an AJAX call
+    // AJAX call - i have a poor understanding of this part
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
+      // variables to contain specific parts of the data being sent over from OPEN WEATHER MAP API
       var weatherTemp = response.main.temp;
       var weatherType = response.weather[0].main;
-
-      // var holdTemp = $(".weatherTemp").text("Temp: " + weatherTemp);
-      // var holdType = $(".weatherType").text("Type: " + weatherType);
-
+      
+      // this sticks variables storing the API info into the variables linked to the CSS
       weatherTempCSS.text("Temp: " + weatherTemp);
       weatherTypeCSS.text("Type: " + weatherType);
       console.log(response);
       
+
+      // this enables a condition for when the API returns odd responses I dont have preprogrammed images for
       var weatherMatch = false;
 
 
-      //looping through array to find match, changes flag from false to true if match is found
+      //looping through array of objects for images to find match, changes flag from false to true if match is found
       for (var i = 0; i < imageData.length; i++) {
         console.log(imageData[i].type);
         if (imageData[i].type === weatherType) {
@@ -118,16 +63,22 @@ $(document).on("click", ".weatherZipButton", function () {
           break;
         }
       };
-// creates imag in image div wether flag changes or not
+// creates image in image div wether flag changes or not
     var weatherImage = $("<img style='width:100%'>")    
       if (weatherMatch === true) {
         weatherImage.attr("src", weatherTypeImage);
       } else {
+        // this is the else where if no matches are found, condition stays false and it kicks in this default image of mysterio instead of a custom character for the weather
         weatherImage.attr("src", "assets/images/otherWeather.png");
       }
+      // this empties the image div to make sure it is clear before displaying an image so that especially if you start searching multiple ZIP's it doesn't append the images together showing multiple images at once
       weatherImageDiv.empty();
       weatherImageDiv.append(weatherImage);
 
     });
 
 });
+
+//===============================================================================================================
+//======================================== END WEATHER STUFF ====================================================
+//===============================================================================================================
